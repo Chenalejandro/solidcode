51ef055c695cb61e3565383fc7c5ad006c900936
alejandro <alejandro@localhost.localdomain>
2024-10-02 18:57:08 -0300

    adding type definition to open-next.config.ts

--- apps/website/open-next.config.ts
+++ apps/website/open-next.config.ts
@@ -1,8 +1,12 @@
-export default {
+import { type OpenNextConfig } from "open-next/types/open-next.js";
+
+const config = {
   default: {
     override: { wrapper: "aws-lambda-streaming" },
   },
   middleware: {
     external: true,
   },
-};
+} satisfies OpenNextConfig;
+
+export default config;
