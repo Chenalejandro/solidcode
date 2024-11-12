cc9316e219a0acfca99c9dcc5f771e1b0e94d2cb
alejandro <alejandro@localhost.localdomain>
2024-11-12 12:17:40 -0300

    changing the posthog persistence to localStorage

--- apps/website/src/app/_analytics/provider.tsx
+++ apps/website/src/app/_analytics/provider.tsx
@@ -9,6 +9,7 @@ import { getUserInfo } from "../[locale]/actions";
 if (env.NEXT_PUBLIC_ENABLE_POSTHOG === "true") {
   if (typeof window !== "undefined") {
     posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+      persistence: "localStorage",
       api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
       capture_pageview: false,
