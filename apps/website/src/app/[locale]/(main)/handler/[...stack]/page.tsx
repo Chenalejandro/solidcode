f67ea2e9a8cca7bf56d182a026c2a2b80d8d756d
alejandro <alejandro@localhost.localdomain>
2024-11-14 12:06:07 -0300

    fixing eslint

--- apps/website/src/app/[locale]/(main)/handler/[...stack]/page.tsx
+++ apps/website/src/app/[locale]/(main)/handler/[...stack]/page.tsx
@@ -3,5 +3,6 @@ import { stackServerApp } from "@/stack";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Handler(props: any) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   return <StackHandler fullPage app={stackServerApp} routeProps={props} />;
 }
