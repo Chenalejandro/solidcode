e8757bb0f483654f85efac10283502a3c3a5f0d9
alejandro <alejandro@localhost.localdomain>
2024-11-14 12:16:08 -0300

    adding loading.tsx

---
+++ apps/website/src/app/[locale]/(main)/loading.tsx
@@ -0,0 +1,6 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  // You can use any loading indicator here
+  return <Skeleton className="min-h-0 flex-grow"></Skeleton>;
+}
