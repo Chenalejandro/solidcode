12a07dc37a3f7e6d8a4df083cd4bfa8c11de42dc
alejandro <alejandro@localhost.localdomain>
2025-02-18 00:20:57 -0300

    updating packages and sending sentry events through tunnelling

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -85,7 +85,7 @@ const finalConfig =
         // This can increase your server load as well as your hosting bill.
         // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
         // side errors will fail.
-        // tunnelRoute: "/monitoring",
+        tunnelRoute: "/monitoring-tunnel",
 
         // Automatically tree-shake Sentry logger statements to reduce bundle size
         disableLogger: true,
