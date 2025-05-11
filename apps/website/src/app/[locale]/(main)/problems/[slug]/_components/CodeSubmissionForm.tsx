4f7ae49dbb4895ed74b6ebea2c315bc96778c378
Alejandro Chen <chen.alejandro97@protonmail.com>
2025-05-11 10:05:55 -0300

    removing useEffect by lifting the useActionState up

--- apps/website/src/app/[locale]/(main)/problems/[slug]/_components/CodeSubmissionForm.tsx
+++ apps/website/src/app/[locale]/(main)/problems/[slug]/_components/CodeSubmissionForm.tsx
@@ -1,13 +1,5 @@
-import {
-  useState,
-  useActionState,
-  type Dispatch,
-  type SetStateAction,
-  useEffect,
-} from "react";
+import { useState, type ReactNode } from "react";
 import { useTheme } from "next-themes";
-import { codeSubmissionAction } from "@/app/[locale]/(main)/problems/[slug]/_actions/CodeSubmissionAction";
-import { toast } from "sonner";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import { useFormStatus } from "react-dom";
@@ -16,126 +8,40 @@ import { ExclamationTriangleIcon } from "@heroicons/react/16/solid";
 import { type ZodError } from "zod";
 import { type LanguagesSchema } from "@/server/data/languages-dto";
 import { ProgrammingLanguageSelector } from "@/app/[locale]/(main)/problems/[slug]/_components/programming-language-selector";
-import LoginModal from "@/components/LoginModal";
-import { type CodeTemplates } from "@/server/data/problems-dto";
-import { type ClientUser } from "../page";
 import dynamic from "next/dynamic";
+import { findSelectedLanguage } from "../utils";
 
 const Editor = dynamic(() => import("@/components/editor"), {
   ssr: false,
 });
 
 export function CodeSubmissionForm({
-  problemId,
   languages,
-  codeTemplates,
-  user,
-  onNewSubmission,
-  onSuccessSubmissionResponse,
   isPoolingSubmissionResult,
-  setIsXXXPending,
+  action,
+  loginModal,
+  currentSelectedLanguageId,
+  onSelectLanguageChange,
+  init,
+  code,
+  onCodeChange,
 }: {
-  problemId: number;
   languages: LanguagesSchema;
-  codeTemplates: CodeTemplates;
-  user: ClientUser;
-  onNewSubmission: (newPublicId?: string) => void;
-  onSuccessSubmissionResponse: () => void;
   isPoolingSubmissionResult: boolean;
-  setIsXXXPending: Dispatch<SetStateAction<boolean>>;
+  action: () => void;
+  loginModal: ReactNode;
+  currentSelectedLanguageId: number;
+  onSelectLanguageChange: (selectedLanguageId: number) => void;
+  init: string;
+  code: string;
+  onCodeChange: (value: string) => void;
 }) {
   const { resolvedTheme } = useTheme();
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false);
   const [monacoEditorMounted, setMonacoEditorMounted] =
     useState<boolean>(false);
-  const [currentSelectedLanguageId, setCurrentSelectedLanguageId] =
-    useState<number>(() => {
-      if (typeof window !== "undefined") {
-        const storedlanguageIdString = localStorage.getItem(
-          `problem${problemId}-languageId`,
-        );
-        if (storedlanguageIdString) {
-          return parseInt(storedlanguageIdString);
-        }
-        return languages[0].id;
-      }
-      return languages[0].id;
-    });
-
-  const [init, setInit] = useState<string>(() => {
-    if (typeof window !== "undefined") {
-      const storedCode = localStorage.getItem(`problem${problemId}-code`);
-      if (storedCode) {
-        return storedCode;
-      }
-      const codeTemplate = findCodeTemplateFromLanguageId(
-        codeTemplates,
-        currentSelectedLanguageId,
-        languages,
-      );
-      return codeTemplate.submissionCode;
-    }
-    const codeTemplate = findCodeTemplateFromLanguageId(
-      codeTemplates,
-      currentSelectedLanguageId,
-      languages,
-    );
-    return codeTemplate.submissionCode;
-  });
-  const [code, setCode] = useState<string>(init);
-
-  const [, action, isXXXPending] = useActionState(async () => {
-    localStorage.setItem(
-      `problem${problemId}-languageId`,
-      currentSelectedLanguageId.toString(),
-    );
-    localStorage.setItem(`problem${problemId}-code`, code);
-    if (!user) {
-      setIsLoginModalOpen(true);
-      return;
-    }
-    const selectedLanguage = findSelectedLanguage(
-      currentSelectedLanguageId,
-      languages,
-    );
-    try {
-      const response = await codeSubmissionAction({
-        problemId: problemId,
-        code,
-        languageId: selectedLanguage.id,
-      });
-      if (response.status === "error") {
-        toast.error(response.problem.title);
-        return;
-      }
-      onSuccessSubmissionResponse();
-      onNewSubmission(response.public_id);
-    } catch (error) {
-      toast.error("Error. The submission api backed is not deployed yet.");
-    }
-  }, null);
-
-  useEffect(() => {
-    if (isXXXPending) {
-      setIsXXXPending(true);
-    } else {
-      setIsXXXPending(false);
-    }
-  }, [isXXXPending]);
-
-  const onSelectLanguageChange = (selectedLanguageId: number) => {
-    setCurrentSelectedLanguageId(selectedLanguageId);
-    const codeTemplate = findCodeTemplateFromLanguageId(
-      codeTemplates,
-      selectedLanguageId,
-      languages,
-    );
-    setInit(codeTemplate.submissionCode);
-    setCode(codeTemplate.submissionCode);
-  };
   return (
     <>
-      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} />
+      {loginModal}
       <form className="flex h-full grow flex-col" action={action}>
         <ProgrammingLanguageSelector
           languages={languages}
@@ -154,9 +60,7 @@ export function CodeSubmissionForm({
               .monacoName
           }
           theme={resolvedTheme === "light" ? "vs" : "vs-dark"}
-          onChange={(value: string) => {
-            setCode(value);
-          }}
+          onChange={onCodeChange}
         />
         <BottomBar
           code={code}
@@ -226,43 +130,9 @@ function SchemaValidationError({ errorInCode }: { errorInCode: ZodError }) {
   return (
     <>
       <ExclamationTriangleIcon className="h-4 w-4 self-center fill-amber-800" />
-      <div className="self-center pl-1 pr-4 text-red-800" role="alert">
+      <div className="self-center pr-4 pl-1 text-red-800" role="alert">
         {error}
       </div>
     </>
   );
-}
-
-function findSelectedLanguage(
-  selectedLanguageId: number,
-  languages: LanguagesSchema,
-) {
-  const selectedLanguage = languages.find(
-    (language) => language.id === selectedLanguageId,
-  );
-  if (!selectedLanguage) {
-    // This condition should never be true
-    throw new Error("The selected language is not valid");
-  }
-  return selectedLanguage;
-}
-
-function findCodeTemplate(codeTemplates: CodeTemplates, languageId: number) {
-  const codeTemplate = codeTemplates.find(
-    (template) => template.languageId === languageId,
-  );
-  if (!codeTemplate) {
-    // This condition should never be true
-    throw new Error("The selected language is not valid");
-  }
-  return codeTemplate;
-}
-
-function findCodeTemplateFromLanguageId(
-  codeTemplates: CodeTemplates,
-  languageId: number,
-  languages: LanguagesSchema,
-) {
-  const selectedLanguage = findSelectedLanguage(languageId, languages);
-  return findCodeTemplate(codeTemplates, selectedLanguage.id);
 }
